d8165a2ac01a474b49830691ff314394c7da8b47
ROHIT YADAV <149073725+R0hit-Yadav@users.noreply.github.com>
2024-07-14 23:38:11 +0530

    Delete Final Round(Library Management System)/Library Managment System directory

--- Final Round(Library Management System)/Library Managment System/FinalRound/FinalRound/book_project/library/forms.py
+++
@@ -1,5 +0,0 @@
-# forms.py
-from django import forms
-
-class BookSearchForm(forms.Form):
-    query = forms.CharField(label='Search for a book', max_length=255)
--- Final Round(Library Management System)/Library Managment System/Library Mangment System/assets/js/readmore.js
+++
@@ -1,7 +0,0 @@
-const readMoreBtn = document.querySelector('.read-more-btn');
-const hiddenContent = document.querySelector('.hidden-content');
-
-readMoreBtn.addEventListener('click', () => {
-  hiddenContent.style.display = hiddenContent.style.display === 'none' ? 'block' : 'none';
-  readMoreBtn.textContent = readMoreBtn.textContent === 'Read More' ? 'Read Less' : 'Read More';
-});
